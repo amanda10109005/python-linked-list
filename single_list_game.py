dc32beb9731b722f369d109299e16c5a2545c388
amanda10109005 <133625869+amanda10109005@users.noreply.github.com>
2023-06-11 22:33:09 +0700

    Add files via upload

---
+++ single_list_game.py
@@ -0,0 +1,93 @@
+#Amanda RIsmawati
+#10109005
+#Soal 3: Menambahkan item ke dalam tas, menghapus item dan mencetak item berdasarkan tingkat kepentingan.
+
+class Item:
+    def _init_(self, nama, kepentingan):
+        self.nama = nama
+        self.kepentingan = kepentingan
+        self.next = None
+
+class Tas:
+    def _init_(self):
+        self.head = None
+
+    def tambahkan_item(self, nama, kepentingan):
+        item_baru = Item(nama, kepentingan)
+
+        if self.head is None:
+            self.head = item_baru
+        else:
+            current_item = self.head
+            if item_baru.kepentingan > current_item.kepentingan:
+                item_baru.next = current_item
+                self.head = item_baru
+            else:
+                while (current_item.next is not None and
+                       item_baru.kepentingan <= current_item.next.kepentingan):
+                    current_item = current_item.next
+                item_baru.next = current_item.next
+                current_item.next = item_baru
+
+    def hapus_item(self, nama):
+        current_item = self.head
+
+        if current_item is not None:
+            if current_item.nama == nama:
+                self.head = current_item.next
+                return
+
+        while current_item is not None:
+            if current_item.nama == nama:
+                break
+            prev_item = current_item
+            current_item = current_item.next
+
+        if current_item is None:
+            return
+
+        prev_item.next = current_item.next
+
+    def cetak_daftar_item(self):
+        current_item = self.head
+
+        if current_item is None:
+            print("Tas kosong")
+        else:
+            print("Daftar item dalam tas:")
+            while current_item is not None:
+                print("Nama: {}, Kepentingan: {}".format(current_item.nama, current_item.kepentingan))
+                current_item = current_item.next
+
+# Contoh penggunaan
+tas = Tas()
+
+while True:
+    print("\nMenu:")
+    print("1. Menambahkan item ke dalam tas")
+    print("2. Menghapus item dari tas")
+    print("3. Cetak daftar item dalam tas")
+    print("4. Keluar")
+
+    pilihan = input("Pilih menu (1/2/3/4): ")
+
+    if pilihan == "1":
+        nama = input("Masukkan nama item: ")
+        kepentingan = int(input("Masukkan tingkat kepentingan item: "))
+        tas.tambahkan_item(nama, kepentingan)
+        print("Item selesai ditambahkan ke dalam tas!")
+
+    elif pilihan == "2":
+        nama = input("Masukkan nama item yang ingin dihapus: ")
+        tas.hapus_item(nama)
+        print("Item ini dihapus dari tas!")
+
+    elif pilihan == "3":
+        tas.cetak_daftar_item()
+
+    elif pilihan == "4":
+        print("Terima kasih!")
+        break
+
+    else:
+        print("Pilihan tidak valid. Silakan pilih menu yang tersedia.") 
